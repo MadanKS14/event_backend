9a4a33b6f97872b46cd26411a3ebd5817bfa1a15
MadanKS14 <madanks1413@gmail.com>
2025-10-22 22:58:39 +0530

    na

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,11 +9,19 @@ const generateToken = (id) => {
 };
 
 
+ 
  const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "Please add all fields" });
+  const { name, email, password, role } = req.body;
+
+ 
+  if (!name || !email || !password || !role) {
+    return res.status(400).json({ message: "Please add name, email, password, and role" });
+  }
+
+
+  if (!['user', 'admin'].includes(role)) {
+      return res.status(400).json({ message: 'Invalid role specified. Must be "user" or "admin".' });
   }
 
   try {
@@ -21,8 +29,7 @@ const generateToken = (id) => {
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
-
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password, role });
 
     res.status(201).json({
       _id: user.id,
@@ -35,6 +42,8 @@ const generateToken = (id) => {
     res.status(400).json({ message: "Invalid user data", error: error.message });
   }
 };
+
+
 
 
  const loginUser = async (req, res) => {
